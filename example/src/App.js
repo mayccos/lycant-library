a8cd5ca3aab1a1fce6baf83676add2a9f06c09ad
jean-christophe Becker <mothos@hotmail.fr>
2022-06-28 19:45:30 +0200

    add component + test

--- example/src/App.js
+++ example/src/App.js
@@ -3,8 +3,35 @@ import React from 'react'
 import { ExampleComponent } from 'lycant-library'
 import 'lycant-library/dist/index.css'
 
+const departments = [
+  {
+    option: 'Sales'
+  },
+  {
+    option: 'Marketing'
+  },
+  {
+    option: 'Engineering'
+  },
+  {
+    option: 'Human Resources'
+  },
+  {
+    option: 'Legal'
+  }
+]
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <ExampleComponent
+      selectName='departments'
+      defaultValue='select a department...'
+      options={departments}
+      label=''
+      selectStyle={{}}
+      wrapperStyle={{}}
+    />
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
 import React from 'react'
 import styles from './styles.module.css'
+import PropTypes from 'prop-types'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ExampleComponent = ({
+  defaultValue,
+  options,
+  label,
+  selectName,
+  wrapperStyle,
+  selectStyle
+}) => {
+  return (
+    <div className={styles.dropdownSelect} style={wrapperStyle}>
+      <select
+        label={label}
+        name={selectName}
+        id={selectName}
+        className={styles.selectName}
+        style={selectStyle}
+      >
+        <option value=''>{defaultValue}</option>
+        {options.map((option) => {
+          return (
+            <option
+              value={option.value ? option.value : option.option}
+              key={option.option}
+            >
+              {option.option}
+            </option>
+          )
+        })}
+      </select>
+    </div>
+  )
+}
+// Prop-Types
+ExampleComponent.propTypes = {
+  label: PropTypes.string,
+  defaultValue: PropTypes.string,
+  options: PropTypes.array,
+  wrapperStyle: PropTypes.object,
+  selectStyle: PropTypes.object
 }
